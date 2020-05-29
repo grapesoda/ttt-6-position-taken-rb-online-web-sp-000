5ad241ec74e59a04118e5b7b840e556e8195cfac
Tres Tarwater <trest03@gmail.com>
2020-05-29 17:38:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-  if array[index_number] == " "
+  if board[index] == " "
    puts false
-  elsif array[index_number] == ""
+  elsif board[index] == ""
    puts false
 end
 end
